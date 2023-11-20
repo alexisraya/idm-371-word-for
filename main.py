ba48afabdd2502147a55bfb42984eeec0dbdf5a2
Alexis Raya <aar335@drexel.edu>
2023-11-19 22:36:58 -0500

    :tada: add starter React files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langchain.chat_models import ChatOpenAI
 
+llm = ChatOpenAI(temperature=0.8, openai_api_key="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", model_name='gpt-4', request_timeout=1000) #gitignore
 user_input = input("Please enter a word in English to be translated to Spanish: \n")
 
 prompt = 'Please translate "' + user_input + '" from English to Spanish'
