2608711eff47262c6314a69c561aac1fe492eac6
Alexis Raya <aar335@drexel.edu>
2023-11-26 19:28:45 -0500

    :hammer: cleanup code

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
 from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv
 
-llm = ChatOpenAI(temperature=0.8, openai_api_key="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", model_name='gpt-4', request_timeout=1000) #gitignore
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI(temperature=0.8, openai_api_key=api_key, model_name='gpt-4', request_timeout=1000)
 user_input = input("Please enter a word in English to be translated to Spanish: \n")
 
 prompt = 'Please translate "' + user_input + '" from English to Spanish'
