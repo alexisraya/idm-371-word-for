25b204a96729b6af13b05f2f4f507b0d39bcf0e9
Alexis Raya <aar335@drexel.edu>
2024-01-21 23:17:57 -0500

    :lipstick: begin colors list

---
+++ src/lib/constants/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+    // countries: Argentina, Bolivia, Chile, Colombia, Costa Rica, Cuba, Dominican Republic, Ecuador, El Salvador, Equatorial Guinea., Guatemala, Honduras, Mexico, Nicaragua, Panama, Paraguay, Peru, Puerto Rico, Uruguay, Venezuela,Spain
+    argentina: {
+        blue: `#5BA2DA`,
+        white: `#FFFFFF`,
+        yellow: `#FFC50E`
+    },
+    bolivia: {
+        red: `#DD2235`,
+        yellow: `#FCE300`,
+        green: `#006F3A`,
+    }
+}
