c940ed2a7a0a4b4d6832a42d25e306ab19d7bd74
Alexis Raya <aar335@drexel.edu>
2024-01-23 13:00:29 -0500

    :lipstick: update colors

--- src/lib/constants/colors.ts
+++ src/lib/constants/colors.ts
@@ -1,13 +1,31 @@
 export const COLORS = {
+    primary_black: `#000000`,
+    text_black: `#141414`,
+    stone_grey: `#979797`,
+    cloud_grey: `#F0F0F0`,
+    white: `#FFFFFF`,
+    dark_blue: `#1D45A2`,
+    navy: `#023871`,
+    medium_blue: `#006DCA`,
+    light_blue: `#5BA2DA`,
+    turquoise: `#00BCE4`,
+    light_yellow: `#FCE300`,
+    dark_yellow: `#FFC50E`,
+    orange: `#FF8200`,
+    red: `#DD2235`,
+    green: `#006F3A`
+}
+
+export const COUNTRY_COLORS = {
     // countries: Argentina, Bolivia, Chile, Colombia, Costa Rica, Cuba, Dominican Republic, Ecuador, El Salvador, Equatorial Guinea., Guatemala, Honduras, Mexico, Nicaragua, Panama, Paraguay, Peru, Puerto Rico, Uruguay, Venezuela,Spain
     argentina: {
-        blue: `#5BA2DA`,
-        white: `#FFFFFF`,
-        yellow: `#FFC50E`
+        blue: COLORS.light_blue,
+        white: COLORS.white,
+        yellow: COLORS.dark_yellow
     },
     bolivia: {
-        red: `#DD2235`,
-        yellow: `#FCE300`,
-        green: `#006F3A`,
+        red: COLORS.red,
+        yellow: COLORS.light_yellow,
+        green: COLORS.green,
     }
 }
