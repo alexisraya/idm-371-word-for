278ae3ea9256a6aaf726b2698e4e69915ccadcd5
Alexis Raya <aar335@drexel.edu>
2024-02-21 21:35:57 -0500

    :bug: fix apostrophe bug

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -50,7 +50,6 @@ declare module '$env/static/private' {
 	export const PATH: string;
 	export const npm_package_json: string;
 	export const _: string;
-	export const LaunchInstanceID: string;
 	export const npm_config_userconfig: string;
 	export const npm_config_init_module: string;
 	export const USER_ZDOTDIR: string;
@@ -61,6 +60,7 @@ declare module '$env/static/private' {
 	export const EDITOR: string;
 	export const npm_package_name: string;
 	export const LANG: string;
+	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 	export const XPC_FLAGS: string;
 	export const npm_config_node_gyp: string;
 	export const npm_package_version: string;
@@ -68,11 +68,14 @@ declare module '$env/static/private' {
 	export const VSCODE_INJECTION: string;
 	export const SHLVL: string;
 	export const HOME: string;
+	export const VSCODE_GIT_ASKPASS_MAIN: string;
 	export const npm_config_cache: string;
 	export const LOGNAME: string;
 	export const npm_lifecycle_script: string;
+	export const VSCODE_GIT_IPC_HANDLE: string;
 	export const npm_config_user_agent: string;
-	export const SECURITYSESSIONID: string;
+	export const VSCODE_GIT_ASKPASS_NODE: string;
+	export const GIT_ASKPASS: string;
 	export const npm_node_execpath: string;
 	export const npm_config_prefix: string;
 	export const COLORTERM: string;
@@ -130,7 +133,6 @@ declare module '$env/dynamic/private' {
 		PATH: string;
 		npm_package_json: string;
 		_: string;
-		LaunchInstanceID: string;
 		npm_config_userconfig: string;
 		npm_config_init_module: string;
 		USER_ZDOTDIR: string;
@@ -141,6 +143,7 @@ declare module '$env/dynamic/private' {
 		EDITOR: string;
 		npm_package_name: string;
 		LANG: string;
+		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
 		XPC_FLAGS: string;
 		npm_config_node_gyp: string;
 		npm_package_version: string;
@@ -148,11 +151,14 @@ declare module '$env/dynamic/private' {
 		VSCODE_INJECTION: string;
 		SHLVL: string;
 		HOME: string;
+		VSCODE_GIT_ASKPASS_MAIN: string;
 		npm_config_cache: string;
 		LOGNAME: string;
 		npm_lifecycle_script: string;
+		VSCODE_GIT_IPC_HANDLE: string;
 		npm_config_user_agent: string;
-		SECURITYSESSIONID: string;
+		VSCODE_GIT_ASKPASS_NODE: string;
+		GIT_ASKPASS: string;
 		npm_node_execpath: string;
 		npm_config_prefix: string;
 		COLORTERM: string;
