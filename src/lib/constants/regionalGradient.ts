2d33c097d1b493b439974019efdf64483bd6c6d3
Dane LeGassick <danelegassick@gmail.com>
2024-03-04 20:36:13 -0500

    TS and Gradients folder
    Added object for regional gradients
    
    Added folder for gradients in assets
    
    Example gradient for bolivia and default

---
+++ src/lib/constants/regionalGradient.ts
@@ -0,0 +1,69 @@
+export const REGIONAL_GRADIENTS = {
+    // countries: Argentina, Bolivia, Chile, Colombia, Costa Rica, Cuba, Dominican Republic, Ecuador, El Salvador, Equatorial Guinea., Guatemala, Honduras, Mexico, Nicaragua, Panama, Paraguay, Peru, Puerto Rico, Uruguay, Venezuela, Spain
+    default: {
+        src: "$lib/assets/gradients/default.svg"
+    },
+    argentina: {
+        src: "$lib/assets/gradients/argentina.svg"
+    },
+    bolivia: {
+        src: "$lib/assets/gradients/bolivia.svg"
+    },
+    chile: {
+        src: "$lib/assets/gradients/chile.svg"
+    },
+    columbia: {
+        src: "$lib/assets/gradients/columbia.svg"
+    },
+    costaRica: {
+        src: "$lib/assets/gradients/costaRica.svg"
+    },
+    cuba: {
+        src: "$lib/assets/gradients/cuba.svg"
+    },
+    dominicanRepublic: {
+        src: "$lib/assets/gradients/dominicanRepublic.svg"
+    },
+    ecuador: {
+        src: "$lib/assets/gradients/ecuador.svg"
+    },
+    elSalvador: {
+        src: "$lib/assets/gradients/elSalvador.svg"
+    },
+    equitorialGuinea: {
+        src: "$lib/assets/gradients/equitorialGuinea.svg"
+    },
+    guatemala: {
+        src: "$lib/assets/gradients/guatemala.svg"
+    },
+    honduras: {
+        src: "$lib/assets/gradients/honduras.svg"
+    },
+    mexico: {
+        src: "$lib/assets/gradients/mexico.svg"
+    },
+    nicaragua: {
+        src: "$lib/assets/gradients/nicaragua.svg"
+    },
+    panama: {
+        src: "$lib/assets/gradients/panama.svg"
+    },
+    paraguay: {
+        src: "$lib/assets/gradients/paraguay.svg"
+    },
+    peru: {
+        src: "$lib/assets/gradients/peru.svg"
+    },
+    puertoRico: {
+        src: "$lib/assets/gradients/puertoRico.svg"
+    },
+    uruguay: {
+        src: "$lib/assets/gradients/uruguay.svg"
+    },
+    venezuela: {
+        src: "$lib/assets/gradients/venezuela.svg"
+    },
+    spain: {
+        src: "$lib/assets/gradients/spain.svg"
+    },
+}
