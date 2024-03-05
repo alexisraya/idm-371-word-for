aa6dcb5853bedaf75f4c1144c7bb82b4d03a3def
Alexis Raya <aar335@drexel.edu>
2024-03-04 22:40:45 -0500

    :sparkles: create and implement toast messages

---
+++ src/lib/ToastMessage.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import check from '$lib/assets/check.svg'
+    import { onMount } from 'svelte';
+    import { fade } from 'svelte/transition'; 
+
+    export let message: string;
+    let visible = true;
+
+    onMount(async () => {
+        setTimeout(()=>{
+            visible = false;
+        }, 3000);
+    });
+</script>
+
+{#if visible}
+    <article class="toast-container" role="alert" transition:fade>
+        <img class="checkmark" src={check} alt="checkmark" />
+        <p>{message}</p>
+    </article>
+{/if}
+
+<style>
+    .toast-container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #006F3A;
+        height: 44px;
+        width: calc(100vw - 3rem);
+        max-width: 30rem;
+        padding: 0 13px;
+        column-gap: 8px;
+        border-radius: 8px;
+        margin: auto;
+    }
+
+    .checkmark{
+        height: 18px;
+    }
+</style>
