d2b3d9ef81963f6b3898caa1d2f2dbced95aff83
Dane LeGassick <68877373+danelegassick@users.noreply.github.com>
2024-03-08 10:23:48 -0500

    Update Skeleton Loader (#37)
    Changed animation and colors
    
    Rounded corners more

--- src/lib/Skeleton.svelte
+++ src/lib/Skeleton.svelte
@@ -1,13 +1,13 @@
 <div class="container">
     <div class="skeleton-container">
         <div class="skeleton-header-container">
-            <div class="skeleton skeleton-text"></div>
-            <div class="skeleton skeleton-header"></div>
+            <div class="skeleton skeleton-text pulsate"></div>
+            <div class="skeleton skeleton-header pulsate"></div>
         </div>
-        <div class="skeleton skeleton-box"></div>
-        <div class="skeleton skeleton-text"></div>
-        <div class="skeleton skeleton-box"></div>
-        <div class="skeleton skeleton-box"></div>
+        <div class="skeleton skeleton-box pulsate"></div>
+        <div class="skeleton skeleton-text pulsate"></div>
+        <div class="skeleton skeleton-box pulsate"></div>
+        <div class="skeleton skeleton-box pulsate"></div>
     </div>
 </div>
 
@@ -23,30 +23,30 @@
         margin: 1.5rem 0;
     }
 
-    .skeleton {
+    /* .skeleton {
         opacity: .7;
-        animation: skeleton-loading 1s linear infinite alternate;
-    }
+        animation: skeleton-loading 1s linear infinite alternate-reverse;
+    } */
 
     .skeleton-text {
         width: 50%;
         height: .5rem;
-        margin-bottom: .25rem;
-        border-radius: .125rem;
+        margin-bottom: .5rem;
+        border-radius: .5rem;
     }
 
     .skeleton-header {
         width: 75%;
         height: 3rem;
         margin-top: .5rem;
-        border-radius: .125rem;
+        border-radius: .5rem;
     }
 
     .skeleton-box {
         width: 100%;
         height: 10.5rem;
         margin-bottom: 1.5rem;
-        border-radius: .125rem;
+        border-radius: 1rem;
     }
 
     .skeleton-text:last-child {
@@ -54,13 +54,27 @@
         width: 80%;
     }
 
-    @keyframes skeleton-loading {
-    0% {
-        background-color: hsl(200, 20%, 70%);
+    .pulsate {
+        opacity: 0.9;
+        background: linear-gradient(-45deg, #DDDDDD, #F0F0F0, #DDDDDD, #F0F0F0);
+        background-size: 400% 400%;
+        animation: Gradient 2.25s ease-in-out infinite;
     }
 
-    100% {
-        background-color: hsl(200, 20%, 95%);
-    }
-    }
+
+    @keyframes Gradient {
+	0% {
+		background-position: 0% 50%
+	}
+	50% {
+		background-position: 100% 50%
+	}
+	100% {
+		background-position: 0% 50%
+	}
+}
+
+    /* gradient   
+        background: linear-gradient(white, white) padding-box, linear-gradient(135deg,#006DCA, #DD2235, #FF8200, #FFC50E, #006F3A) border-box;
+    */
 </style>
