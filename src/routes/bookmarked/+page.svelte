fe89299f46cff22e5ee28569a2183e23caf4d334
Dane LeGassick <68877373+danelegassick@users.noreply.github.com>
2024-03-11 19:06:49 -0400

    update modal height (#43)
    modal height was slightly too large for mobile screens
    
    this will bring it down a bit to fit better

--- src/routes/bookmarked/+page.svelte
+++ src/routes/bookmarked/+page.svelte
@@ -359,7 +359,7 @@
 
     .modal-container {
         width: 100%;
-        height: 90vh;
+        height: 85vh;
         display: flex;
         flex-direction: column;
         gap: 1rem;
@@ -629,7 +629,7 @@
     }
 
     .spacer {
-        height: calc(90vh - 9rem);
+        height: calc(85vh - 9rem);
         overflow-y: auto;
     }
 
