7c473b26b3d14ff63f30af6dec11accf947429ca
Alexis Raya <aar335@drexel.edu>
2025-09-12 16:13:08 -0400

    Update TypeScript to version 4.9.5
    Bumped TypeScript dependency from 4.9.3 to 4.9.5 in package.json and package-lock.json. Also refactored eslint.config.ts to use single-line config objects for improved readability.

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,17 +5,7 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
-  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
-  },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
 ]);
